0db71595780978fec3145661272610d3d02af5be
KimJongChan <whdcks420@gmail.com>
2023-02-02 01:15:42 +0900

    [unrated] Title: 무인도 여행, Time: 3.04 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+
+    public int[] solution(String[] maps) {
+        char[][] map = new char[maps.length][];
+        boolean[][] visit = new boolean[maps.length][maps[0].length()];
+
+        // map create
+        for (int i = 0; i < maps.length; i++) {
+            map[i] = maps[i].toCharArray();
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int col = 0; col < maps.length; col++) {
+            for (int row = 0; row < maps[0].length(); row++) {
+                int width = findSector(map, visit, col, row);
+                if (width > 0) {
+                    ans.add(width);
+                }
+            }
+        }
+
+        return ans.size() != 0 ? ans.stream().sorted().mapToInt(value -> value).toArray() : new int[]{-1};
+
+    }
+
+    private int findSector(char[][] map, boolean[][] visit, int col, int row) {
+        if (col < 0 || row < 0 || col == map.length || row == map[0].length) {
+            return 0;
+        }
+
+        if (visit[col][row] || map[col][row] == 'X') {
+            return 0;
+        }
+
+        visit[col][row] = true;
+
+        int rst = map[col][row] - '0';
+
+        rst += findSector(map, visit, col - 1, row);
+        rst += findSector(map, visit, col + 1, row);
+        rst += findSector(map, visit, col, row - 1);
+        rst += findSector(map, visit, col, row + 1);
+
+        return rst;
+    }
+}
