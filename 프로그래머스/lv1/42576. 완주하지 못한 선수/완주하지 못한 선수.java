21bc756b9cd08293b084eb3567287c8904a841d4
KimJongChan <whdcks420@gmail.com>
2023-02-06 01:45:35 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.59 ms, Memory: 96 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+
+        String ans = "";
+        for (String s : map.keySet()) {
+            if(map.get(s) != 0) {
+                ans = s;
+            }
+        }
+        return ans;
+    }
+}
