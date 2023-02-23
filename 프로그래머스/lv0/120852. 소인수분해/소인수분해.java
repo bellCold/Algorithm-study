47dc1841e170e23c7fe81fd1b4110a7bff8ff4ee
KimJongChan <whdcks420@gmail.com>
2023-02-24 03:37:38 +0900

    [level 0] Title: 소인수분해, Time: 1.81 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                list.add(i);
+                while (n % i == 0) {
+                    n /= i;
+                }
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
