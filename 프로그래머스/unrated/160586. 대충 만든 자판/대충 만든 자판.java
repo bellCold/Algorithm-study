ce770fb499f94de40106dd19d1efaa0c434c1a5e
KimJongChan <whdcks420@gmail.com>
2023-02-25 01:40:22 +0900

    [unrated] Title: 대충 만든 자판, Time: 11.30 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(String[] keymap, String[] targets) {
+        Map<String, Integer> key = new HashMap<>();
+
+        for (String s : keymap) {
+            String[] split = s.split("");
+            for (int i = 0; i < split.length; i++) {
+                if (key.containsKey(split[i])) {
+                    if (key.get(split[i]) > i + 1) {
+                        key.put(split[i], i + 1);
+                    }
+                } else {
+                    key.put(split[i], i + 1);
+                }
+            }
+        }
+
+        int[] ans = new int[targets.length];
+
+        for (int i = 0; i < targets.length; i++) {
+            int count = 0;
+            String target = targets[i];
+            String[] split = target.split("");
+            boolean flag = true;
+            for (int j = 0; j < split.length; j++) {
+                if (key.containsKey(split[j])) {
+                    count += key.get(split[j]);
+                } else {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                ans[i] = count;
+            } else {
+                ans[i] = -1;
+            } 
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] solution1 = solution.solution(new String[]{"ABC","BCA"}, new String[]{"CB"});
+        for (int i : solution1) {
+            System.out.println(i);
+        }
+    }
+}
