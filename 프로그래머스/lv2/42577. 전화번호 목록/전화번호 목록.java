10c09035dfd314dad1e8164766422b9120eb364a
KimJongChan <whdcks420@gmail.com>
2023-03-16 17:44:58 +0900

    [level 2] Title: 전화번호 목록, Time: 295.26 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,9 +3,8 @@ import java.util.Arrays;
 class Solution {
     public boolean solution(String[] phoneBook) {
         Arrays.sort(phoneBook);
-        
-        for (int i=0; i<phoneBook.length-1; i++) {
-            if (phoneBook[i+1].startsWith(phoneBook[i])) {
+        for (int i = 0; i < phoneBook.length - 1; i++) {
+            if (phoneBook[i + 1].startsWith(phoneBook[i])) {
                 return false;
             }
         }
