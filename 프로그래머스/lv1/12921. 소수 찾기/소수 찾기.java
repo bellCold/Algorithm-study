72c48e4c9cd209c4f75b141016a541f70f36c073
KimJongChan <whdcks420@gmail.com>
2023-03-21 04:40:36 +0900

    [level 1] Title: 소수 찾기, Time: 42.66 ms, Memory: 55.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n) {
+        int[] ari = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            ari[i] = i;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= n; j = j + i) {
+                if (j != i) {
+                    ari[j] = 0;
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
+            if (ari[i] != 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+}
