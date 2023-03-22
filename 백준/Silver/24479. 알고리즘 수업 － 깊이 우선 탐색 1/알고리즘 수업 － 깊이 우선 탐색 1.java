9f52ef43c1419dd6cbe7d8bc3e67ec763f53594f
KimJongChan <whdcks420@gmail.com>
2023-03-22 17:01:58 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1912 ms, Memory: 96692 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -2,56 +2,83 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static ArrayList<Integer>[] node;
+
+    static List<Integer>[] node;
     static boolean[] visited;
-    static int[] answer;
     static int count = 1;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
 
-        node = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        answer = new int[n + 1];
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < n + 1; i++) {
+        node = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
             node[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
+
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
+
             node[a].add(b);
             node[b].add(a);
         }
 
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 1; i <= N; i++) {
             Collections.sort(node[i]);
         }
 
-        dfs(v);
-        for (int i = 1; i < n + 1; i++) {
-            System.out.println(answer[i]);
+        int[] ans = new int[N + 1];
+
+        dfs(node, R, ans);
+
+        for (int i = 1; i < ans.length; i++) {
+            System.out.println(ans[i]);
         }
     }
 
-    private static void dfs(int v) {
-        visited[v] = true;
-        answer[v] = count++;
-        for (int i : node[v]) {
-            if (!visited[i]) {
-                dfs(i);
+    private static void dfs(List<Integer>[] node, int start, int[] ans) {
+        if (visited[start]) {
+            return;
+        }
+
+        /**
+         * 1 4
+         * 1 2
+         * 2 3
+         * 2 4
+         * 3 4
+         */
+
+        /**
+         * ans[1] = 1
+         * ans[2] = 2;
+         * ans[3] = 4;
+         *
+         */
+        ans[start] = count++;
+
+        visited[start] = true;
+
+        for (int num : node[start]) {
+            if (!visited[num]) {
+                dfs(node, num, ans);
             }
         }
     }
+
 }
