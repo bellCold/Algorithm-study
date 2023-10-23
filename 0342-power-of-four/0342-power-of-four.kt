14caa0d7ded412ba01da6c13b2bdeb427c9f79af
kimjongchan <whdcks420@gmail.com>
2023-10-23 16:15:34 +0900

    Time: 146 ms (47.06%), Space: 33.6 MB (51.47%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.kt
+++ 0342-power-of-four/0342-power-of-four.kt
@@ -1,13 +1,17 @@
 class Solution {
     fun isPowerOfFour(n: Int): Boolean {
-        if (n <= 0) {
-            return false
-        }
-        if (n == 1) {
-            return true
-        }
-        if (n % 4 == 0) {
-             return isPowerOfFour(n / 4)
+        n.run {
+            if (n <= 0) {
+                return false
+            }
+        }.run {
+            if (n == 1) {
+                return true
+            }
+        }.run {
+            if (n % 4 == 0) {
+                return isPowerOfFour(n / 4)
+            }
         }
         return false
     }
