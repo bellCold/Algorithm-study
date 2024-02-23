081bb8fc37aa7fdec9de5823552c9dcbdf040090
kimjongchan <whdcks420@gmail.com>
2024-02-23 12:57:28 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14496 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,36 +2,31 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        int people = Integer.parseInt(br.readLine());
 
-        int[] array = new int[n];
-        int[] dp = new int[n];
+        int[] peopleArray = new int[people];
+        int[] S = new int[people];
 
-        String[] split = br.readLine().split(" ");
-
-        for (int i = 0; i < n; i++) {
-            array[i] = Integer.parseInt(split[i]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < peopleArray.length; i++) {
+            peopleArray[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(array);
-        if (n == 1) {
-            System.out.println(array[0]);
-            return;
+        Arrays.sort(peopleArray);
+
+        S[0] = peopleArray[0];
+        for (int i = 1; i < peopleArray.length; i++) {
+            S[i] = peopleArray[i] + S[i - 1];
         }
 
-        dp[0] = array[0];
-        dp[1] = array[1] + array[0];
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = dp[i - 1] + array[i];
-        }
-
-        System.out.println(Arrays.stream(dp).reduce(Integer::sum).getAsInt());
+        System.out.println(Arrays.stream(S).sum());
     }
 
 }
